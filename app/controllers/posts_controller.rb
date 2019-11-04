81da63876af54f396e284cd98841e72c650bed46
Timmy_MT <mathcore_detected@mail.ru>
2019-11-04 13:28:21 +0500

    crud posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,53 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :set_category, only: [:new, :create]
+
+  def show
+  end
+
+  def new
+    @post = @category.posts.new
+  end
+
+  def create
+    @post = @category.posts.new(post_params)
+    if @post.save
+      redirect_to post_path(@post)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+    if @post.destroyed?
+      redirect_to category_path(@post.category)
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   resources :categories, shallow: true do
-    resources :posts
+    resources :posts, shallow: true
   end
 
   root "categories#index"
