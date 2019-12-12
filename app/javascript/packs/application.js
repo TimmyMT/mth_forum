d40bf8b3d3a2aca6288a4323b28c2ff1b9ac3403
Timmy_MT <mathcore_detected@mail.ru>
2019-12-12 15:39:41 +0500

    override scss bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,9 @@
 import "bootstrap"
-import "../stylesheets/application"
-import "core-js/stable";
-import "renegerator-runtime/runtime"
-
-document.addEventListener("turbolinks:load", () => {
-  $('[data-toggle="tooltip"]').tooltip()
-  $('[data-toggle="popover"]').popover()
-})
+// import "../stylesheets/application"
+// import "core-js/stable";
+// import "renegerator-runtime/runtime"
+//
+// document.addEventListener("turbolinks:load", () => {
+//   $('[data-toggle="tooltip"]').tooltip()
+//   $('[data-toggle="popover"]').popover()
+// })
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,29 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require("webpack")
 
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-  $: 'jquery',
-  jQuery: 'jquery',
-  Popper: ['popper.js', 'default']
-}))
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default']
+    })
+)
+
+environment.loaders.append('expose', {
+  test: require.resolve('jquery'),
+  use: [{
+    loader: 'expose-loader',
+    options: '$'
+  }]
+})
+
+// environment.plugins.append("Provide", new webpack.ProvidePlugin({
+//   $: 'jquery',
+//   jQuery: 'jquery',
+//   Popper: ['popper.js', 'default']
+// }))
 
 module.exports = environment
